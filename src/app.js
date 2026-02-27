ead29c858ad23077ccae853a26879172cddc4a94
Pranav <bizzareforyou@gmail.com>
2026-02-27 21:48:22 +0530

    initialized database by creating a user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/userSchema");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "abc",
+    lastName: "def",
+    age: 99,
+    gender: "male",
+    profession: "unemployed",
+  };
+
+  const user = new User(userObj);
+
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(400).send("Couldn't save the user");
+  }
+});
 
 connectDB()
   .then(() => {
---
+++ src/models/userSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+  profession: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
