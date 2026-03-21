7621655ba1cfd9ec651e1a8a4af346f69d8f4bac
Pranav <bizzareforyou@gmail.com>
2026-03-21 21:30:26 +0530

    added chat router

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const userRouter = require("./routes/userRouter");
 const cors = require("cors");
 const { createServer } = require("http");
 const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chatRouter");
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
 const server = createServer(app);
 
